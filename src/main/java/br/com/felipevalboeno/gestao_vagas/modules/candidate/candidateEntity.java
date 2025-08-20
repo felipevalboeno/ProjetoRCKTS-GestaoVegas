b66393bf703b66c7bd777f421db2a9224384d1e8
Felipe Valboeno <feliperocha14@hotmail.com>
2025-08-20 14:30:16 -0300

    Primeiro commit - projeto criado

---
+++ src/main/java/br/com/felipevalboeno/gestao_vagas/modules/candidate/candidateEntity.java
@@ -0,0 +1,16 @@
+package br.com.felipevalboeno.gestao_vagas.modules.candidate;
+
+import java.util.UUID;
+
+public class candidateEntity {
+
+    private UUID id;
+    private String name;
+    private String username;
+    private String email;
+    private String password;
+    private String description;
+    private String curriculum;
+
+    
+}
---
+++ src/main/java/br/com/felipevalboeno/gestao_vagas/modules/candidate/controllers/CandidateController.java
@@ -0,0 +1,10 @@
+package br.com.felipevalboeno.gestao_vagas.modules.candidate.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/candidate")
+public class CandidateController {
+    
+}
