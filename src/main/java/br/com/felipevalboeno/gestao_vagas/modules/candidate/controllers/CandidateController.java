5e97bab0adfa6e147e5d95d60911d578d6d8ac0f
Felipe Valboeno <feliperocha14@hotmail.com>
2025-08-21 14:09:46 -0300

    UseCase - Handling exception in user creation

--- src/main/java/br/com/felipevalboeno/gestao_vagas/modules/candidate/controllers/CandidateController.java
+++ src/main/java/br/com/felipevalboeno/gestao_vagas/modules/candidate/controllers/CandidateController.java
@@ -1,27 +1,41 @@
 package br.com.felipevalboeno.gestao_vagas.modules.candidate.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.felipevalboeno.gestao_vagas.modules.candidate.CandidateEntity;
+import br.com.felipevalboeno.gestao_vagas.modules.candidate.useCases.CreateCandidateUseCase;
 import jakarta.validation.Valid;
 
 
 @RestController
 @RequestMapping("/candidate")
 public class CandidateController {
+ 
 
-
+  @Autowired
+  private CreateCandidateUseCase createCandidateUseCase;
    
     @PostMapping("/")
-    public void create(@Valid @RequestBody CandidateEntity candidateEntity){
-        System.out.println("Candidato");
-        System.out.println(candidateEntity.getEmail());
-          System.out.println(candidateEntity.getName());
-            System.out.println(candidateEntity.getDescription());
-              System.out.println(candidateEntity.getUsername());
+    public ResponseEntity<Object> create(@Valid @RequestBody CandidateEntity candidateEntity){
+      
+      try {
+        var result = this.createCandidateUseCase.execute(candidateEntity);
+        return ResponseEntity.ok().body(result);
+      } catch (Exception e) {
+        
+        return ResponseEntity.badRequest().body(e.getMessage());
+      }
+      
+      
+      
+      
+   
+
 
     }
     
