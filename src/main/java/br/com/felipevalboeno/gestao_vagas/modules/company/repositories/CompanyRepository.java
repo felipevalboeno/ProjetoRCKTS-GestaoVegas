e03d6a1864041c5d453ef8ce50e32dcd2c5675ca
Felipe Valboeno <feliperocha14@hotmail.com>
2025-08-22 15:53:19 -0300

    Spring Secure Authorization - part 2

--- src/main/java/br/com/felipevalboeno/gestao_vagas/modules/company/repositories/CompanyRepository.java
+++ src/main/java/br/com/felipevalboeno/gestao_vagas/modules/company/repositories/CompanyRepository.java
@@ -8,8 +8,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import br.com.felipevalboeno.gestao_vagas.modules.company.entities.CompanyEntity;
 
+
+
 public interface CompanyRepository  extends JpaRepository<CompanyEntity, UUID>{
     
         Optional<CompanyEntity> findByUsernameOrEmail(String username, String email);
 
+        Optional<CompanyEntity> findByUsername(String username);
 }
---
+++ src/main/java/br/com/felipevalboeno/gestao_vagas/modules/company/useCases/AuthCompanyUseCase.java
@@ -0,0 +1,56 @@
+package br.com.felipevalboeno.gestao_vagas.modules.company.useCases;
+
+import javax.crypto.SecretKey;
+import javax.naming.AuthenticationException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+
+import br.com.felipevalboeno.gestao_vagas.modules.company.dto.AuthCompanyDTO;
+import br.com.felipevalboeno.gestao_vagas.modules.company.repositories.CompanyRepository;
+
+@Service
+public class AuthCompanyUseCase {
+
+    @Value("${security.token.secret}")
+    private String secretKey;
+
+    
+@Autowired
+private CompanyRepository companyRepository;
+
+@Autowired
+private PasswordEncoder passwordEncoder;
+
+public String execute(AuthCompanyDTO authCompanyDTO) throws AuthenticationException{
+    var company = this.companyRepository.findByUsername(authCompanyDTO.getUsername())
+    .orElseThrow(
+        () ->{
+            throw new UsernameNotFoundException("Username/Password incorrect");
+        });
+
+        var passwordMatches =this.passwordEncoder.matches(authCompanyDTO.getPassword(), company.getPassword());
+
+        //se as senhas forem diferentes, retorna erro
+        if(!passwordMatches){
+            throw new AuthenticationException();
+        }
+        //se forem iguais, retorna token
+       
+        Algorithm algorithm = Algorithm.HMAC256(secretKey);
+        var token = JWT.create().withIssuer("javagas")
+        .withSubject(company.getId().toString())
+        .sign(algorithm);
+    return token;
+
+
+}
+
+
+}
