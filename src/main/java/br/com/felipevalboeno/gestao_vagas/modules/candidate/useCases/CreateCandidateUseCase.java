2e1cf8d4fed5a3c0148e8ec8c9dcc551e8daa296
Felipe Valboeno <feliperocha14@hotmail.com>
2025-08-27 09:44:02 -0300

    Refactor CreateCandidateUseCase to include PasswordEncoder for password encoding

--- src/main/java/br/com/felipevalboeno/gestao_vagas/modules/candidate/useCases/CreateCandidateUseCase.java
+++ src/main/java/br/com/felipevalboeno/gestao_vagas/modules/candidate/useCases/CreateCandidateUseCase.java
@@ -1,6 +1,7 @@
 package br.com.felipevalboeno.gestao_vagas.modules.candidate.useCases;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import br.com.felipevalboeno.gestao_vagas.exceptions.UserFoundException;
@@ -10,8 +11,11 @@ import br.com.felipevalboeno.gestao_vagas.modules.candidate.CandidateRepository;
 @Service
 public class CreateCandidateUseCase {
 
-     @Autowired
+  @Autowired
   private CandidateRepository candidateRepository;
+
+  @Autowired
+  private PasswordEncoder passwordEncoder;
 
     public CandidateEntity execute(CandidateEntity candidateEntity){
 
@@ -22,6 +26,9 @@ public class CreateCandidateUseCase {
       throw new UserFoundException();
 
     });
+
+    var password = passwordEncoder.encode(candidateEntity.getPassword());
+    candidateEntity.setPassword(password);
 
 
       return this.candidateRepository.save(candidateEntity);
