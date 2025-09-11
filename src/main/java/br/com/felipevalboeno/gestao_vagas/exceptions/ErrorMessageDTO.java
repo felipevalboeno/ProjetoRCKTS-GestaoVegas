58fe4bd07e226239282d02b15181f1adad2232e2
Felipe Valboeno <feliperocha14@hotmail.com>
2025-09-11 16:10:52 -0300

    Add NoArgsConstructor annotation to ErrorMessageDTO for default instantiation

--- src/main/java/br/com/felipevalboeno/gestao_vagas/exceptions/ErrorMessageDTO.java
+++ src/main/java/br/com/felipevalboeno/gestao_vagas/exceptions/ErrorMessageDTO.java
@@ -2,9 +2,11 @@ package br.com.felipevalboeno.gestao_vagas.exceptions;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class ErrorMessageDTO {
     private String message;
     private String field;
