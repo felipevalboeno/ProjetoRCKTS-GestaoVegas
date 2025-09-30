4e9cf4fe6a4adb7c78336ff6e5caf920cf5202a5
Felipe Valboeno <feliperocha14@hotmail.com>
2025-09-30 20:44:28 -0300

    change deletemapping route

--- src/main/java/br/com/felipevalboeno/gestao_vagas/modules/company/controllers/JobController.java
+++ src/main/java/br/com/felipevalboeno/gestao_vagas/modules/company/controllers/JobController.java
@@ -1,7 +1,5 @@
 package br.com.felipevalboeno.gestao_vagas.modules.company.controllers;
 
-
-
 import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -33,7 +31,7 @@ import jakarta.validation.Valid;
 @RestController
 @RequestMapping("/company/job")
 public class JobController {
-    
+
     @Autowired
     private CreateJobUseCase createJobUseCase;
 
@@ -46,67 +44,62 @@ public class JobController {
     @PostMapping("/")
     @PreAuthorize("hasRole('COMPANY')") // so a company pode criar vaga
     @Tag(name = "Vagas", description = "Informações sobre vagas")
-    @Operation(summary = "Cadastro de vagas",
-    description = "Endpoint para cadastro de vagas (somente para empresas).")
+    @Operation(summary = "Cadastro de vagas", description = "Endpoint para cadastro de vagas (somente para empresas).")
     @ApiResponses({
-        @ApiResponse(responseCode = "200",
-         content = {@Content(schema = @Schema(implementation = JobEntity.class))} ),
+            @ApiResponse(responseCode = "200", content = {
+                    @Content(schema = @Schema(implementation = JobEntity.class)) }),
     })
 
     @SecurityRequirement(name = "jwt_auth")
-    public ResponseEntity<Object> create(@Valid @RequestBody CreateJobDTO createJobDTO, HttpServletRequest request){
-        
+    public ResponseEntity<Object> create(@Valid @RequestBody CreateJobDTO createJobDTO, HttpServletRequest request) {
+
         var companyId = request.getAttribute("company_id"); // recupera o atriuto
-        
-        //jobEntity.setCompanyId(UUID.fromString(companyId.toString())); //seta o companyId convertendo pra UUID
-        
-       try {
-        var jobEntity = JobEntity.builder()
-            .benefits(createJobDTO.getBenefits())
-            .companyId(UUID.fromString(companyId.toString()))
-            .description(createJobDTO.getDescription())
-            .level(createJobDTO.getLevel())
-            .build(); 
 
+        // jobEntity.setCompanyId(UUID.fromString(companyId.toString())); //seta o
+        // companyId convertendo pra UUID
 
-        var result = this.createJobUseCase.execute(jobEntity);
-        return ResponseEntity.ok().body(result);
+        try {
+            var jobEntity = JobEntity.builder()
+                    .benefits(createJobDTO.getBenefits())
+                    .companyId(UUID.fromString(companyId.toString()))
+                    .description(createJobDTO.getDescription())
+                    .level(createJobDTO.getLevel())
+                    .build();
 
-       } catch (Exception e) {
-        return ResponseEntity.badRequest().body(e.getMessage());
-        
-       }
+            var result = this.createJobUseCase.execute(jobEntity);
+            return ResponseEntity.ok().body(result);
+
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+
+        }
     }
-
 
     @GetMapping("/")
     @PreAuthorize("hasRole('COMPANY')") // so a company pode criar vaga
     @Tag(name = "Vagas", description = "Listagem das vagas")
-    @Operation(summary = "Listagem de vagas",
-    description = "Endpoint para listagem de vagas (somente para empresas).")
+    @Operation(summary = "Listagem de vagas", description = "Endpoint para listagem de vagas (somente para empresas).")
     @ApiResponses({
-        @ApiResponse(responseCode = "200",
-         content = {@Content(schema = @Schema(implementation = JobEntity.class))} ),
+            @ApiResponse(responseCode = "200", content = {
+                    @Content(schema = @Schema(implementation = JobEntity.class)) }),
     })
 
     @SecurityRequirement(name = "jwt_auth")
-    public ResponseEntity<Object> listByCompany(HttpServletRequest request){
+    public ResponseEntity<Object> listByCompany(HttpServletRequest request) {
         var companyId = request.getAttribute("company_id");
         var result = this.listAllJobsByCompanyUseCase.execute(UUID.fromString(companyId.toString()));
         return ResponseEntity.ok().body(result);
     }
 
-
-    @DeleteMapping("/{jobId}")
-@PreAuthorize("hasRole('COMPANY')")
-@Tag(name = "Vagas", description = "Excluir vaga")
-@Operation(summary = "Excluir vaga", description = "Endpoint para excluir vaga (somente para a empresa dona da vaga).")
-@SecurityRequirement(name = "jwt_auth")
-public ResponseEntity<String> deleteJob(@PathVariable UUID jobId, HttpServletRequest request) {
-    UUID companyId = UUID.fromString(request.getAttribute("company_id").toString());
-    deleteJobUseCase.execute(companyId, jobId);
-    return ResponseEntity.ok("Vaga deletada com sucesso");
-}
-
+    @DeleteMapping("/company/jobs/{jobId}")
+    @PreAuthorize("hasRole('COMPANY')")
+    @Tag(name = "Vagas", description = "Excluir vaga")
+    @Operation(summary = "Excluir vaga", description = "Endpoint para excluir vaga (somente para a empresa dona da vaga).")
+    @SecurityRequirement(name = "jwt_auth")
+    public ResponseEntity<String> deleteJob(@PathVariable UUID jobId, HttpServletRequest request) {
+        UUID companyId = UUID.fromString(request.getAttribute("company_id").toString());
+        deleteJobUseCase.execute(companyId, jobId);
+        return ResponseEntity.ok("Vaga deletada com sucesso");
+    }
 
 }
