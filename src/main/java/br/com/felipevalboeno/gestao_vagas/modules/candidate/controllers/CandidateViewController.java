9894316946b91a1783cfa6154d98525d448e0a57
Felipe Valboeno <feliperocha14@hotmail.com>
2025-10-02 15:57:14 -0300

    add

--- src/main/java/br/com/felipevalboeno/gestao_vagas/modules/candidate/controllers/CandidateViewController.java
+++ src/main/java/br/com/felipevalboeno/gestao_vagas/modules/candidate/controllers/CandidateViewController.java
@@ -1,15 +1,34 @@
 package br.com.felipevalboeno.gestao_vagas.modules.candidate.controllers;
 
+import java.util.List;
+import java.util.UUID;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import br.com.felipevalboeno.gestao_vagas.modules.candidate.dto.AppliedJobResponseDTO;
+import br.com.felipevalboeno.gestao_vagas.modules.candidate.useCases.ListJobsAppliedByCandidateUseCase;
+import jakarta.servlet.http.HttpServletRequest;
+
 
 @Controller
 @RequestMapping("/candidate")
 public class CandidateViewController {
 
+    @Autowired
+    private ListJobsAppliedByCandidateUseCase listJobsAppliedByCandidateUseCase;
+
     @GetMapping("/job/applied/view")
-    public String showAppliedJobsPage() {
-        return "candidate/applied-jobs"; // aponta para templates/candidate/applied-jobs.html
+    public String showAppliedJobsPage(Model model, HttpServletRequest request) {
+        // Você pode carregar as vagas aplicadas direto no backend
+        UUID candidateId = UUID.fromString(request.getAttribute("candidate_id").toString());
+        List<AppliedJobResponseDTO> jobs = listJobsAppliedByCandidateUseCase.execute(candidateId);
+        model.addAttribute("jobs", jobs);
+        return "candidate/applied-jobs"; // template Thymeleaf
     }
+
+    
 }
