ab5e10aa0a29f144fda4d731269ba07c322eebed
Felipe Valboeno <feliperocha14@hotmail.com>
2025-10-02 15:36:57 -0300

    add permitall

--- src/main/java/br/com/felipevalboeno/gestao_vagas/security/SecurityConfig.java
+++ src/main/java/br/com/felipevalboeno/gestao_vagas/security/SecurityConfig.java
@@ -37,7 +37,8 @@ public class SecurityConfig {
             .requestMatchers("/company/").permitAll()
             .requestMatchers("/company/auth").permitAll()
             .requestMatchers(PERMIT_ALL_LIST).permitAll()
-            .requestMatchers("/candidate/auth").permitAll();
+            .requestMatchers("/candidate/auth").permitAll()
+            .requestMatchers("/candidate/job/applied/view").permitAll();
             
             auth.anyRequest().authenticated();
         })
