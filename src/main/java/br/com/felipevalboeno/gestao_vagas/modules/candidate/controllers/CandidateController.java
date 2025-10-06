07d1c7085d3c1fc60c60982904e66a2e594db8fa
Felipe Valboeno <feliperocha14@hotmail.com>
2025-10-06 14:44:17 -0300

    add cors origns on candidateController

--- src/main/java/br/com/felipevalboeno/gestao_vagas/modules/candidate/controllers/CandidateController.java
+++ src/main/java/br/com/felipevalboeno/gestao_vagas/modules/candidate/controllers/CandidateController.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -239,7 +240,7 @@ public ResponseEntity<List<AppliedJobResponseDTO>> listAppliedJobs(HttpServletRe
     
 }
 
-
+@CrossOrigin(origins = "*")
 @DeleteMapping("/job/apply/{jobId}")
 @PreAuthorize("hasRole('CANDIDATE')")
 @Operation(
